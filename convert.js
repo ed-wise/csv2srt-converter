4225feecda01aab282bbbe6aee40f12c796ab9dd
Edwin E <edwin.eshuis@e-wise.nl>
2018-10-18 13:24:39 +0200

    Added description + little cleanup

--- convert.js
+++ convert.js
@@ -1,3 +1,12 @@
+/**
+ * Converts Vimeo .csv file to .srt subtitle file
+ * 
+ * Vimeo timecode is formatted  > hh:mm:ss
+ * Vimeo csv files contains 1 timecode field per row
+ * Subtitle timecode is formatted > hh:mm:ss,ms
+ * Subtitle srt files needs 2 timecode fields per row: start + end
+ */
+
 let fs = require('fs');
 let Papa = require('papaparse');
 let tc = require('./custom_lib/timecode.js');
@@ -6,12 +15,10 @@ const newline = '\n';
 const csvFolder = './csv/';
 const srtFolder = './srt/';
 const doneFolder = './csv/done/';
-
 const csvTimeCodeKey = 'Timecode';
 const addTime = '00:00:02';
+const ms = ',000';
 
-
-// let csvFile = csvFolder + 'burnout.csv';
 let myFiles = [];
 
 let start = new Date().getTime();
@@ -28,30 +35,27 @@ if(myFiles.length == 0){
   console.log("Duration in ms: " + elapsed);
 }
 
-/**
- * FUNCTIONS
- */
-
  // Converts all csv files to srt files
 function startConverting(myFiles){
   for (let i = 0; i < myFiles.length; i++) {
-    let contentCsv = readCsvFile(myFiles[i]);
-    writeSrtFile(contentCsv, myFiles[i]);
+    let newCsv = readCsvFile(myFiles[i]);
+    let sortCsv = sortByKey(newCsv, csvTimeCodeKey);
+    let newSrt = createSrtRows(sortCsv);
+    let newSrtFileName = myFiles[i].replace(".csv", ".srt");
+    
+    writeSrtFile(newSrt, newSrtFileName);
     moveFile(myFiles[i]);
   }
 }
 
-// 1. Sort csv array bij timecode
-// 2. Create 
-function writeSrtFile(rows, csvFile){
-  let sortRows    = sortByKey(rows, csvTimeCodeKey);
-  let mySrt       = createSrtRows(sortRows);
-  let srtFileName = csvFile.split(".");
-  fs.writeFile(srtFolder + srtFileName[0] + ".srt", mySrt, function(err) {
+// Write output as srt file
+function writeSrtFile(newSrt, srtFileName){
+  let srtFile = srtFolder + srtFileName;
+  fs.writeFile(srtFile, newSrt, function(err) {
     if(err) {
         return console.log(err);
     }
-    console.log("saved : " + srtFolder + srtFileName[0] + '.srt');
+    console.log("saved : " + srtFile);
   }); 
 }
 
@@ -76,8 +80,9 @@ function readCsvFolder() {
 // Read content of a csv file and convert it to a csv array
 function readCsvFile(csvFile) {
   let csv = csvFolder + csvFile,
-    content = fs.readFileSync(csv, "utf8"),
-    rows;
+      content = fs.readFileSync(csv, "utf8"),
+      rows;
+
   Papa.parse(content, {
     header: true,
     delimiter: "",
@@ -91,16 +96,19 @@ function readCsvFile(csvFile) {
 
 // Convert csv array to subtitle sequences (srt standard)
 function createSrtRows(csvArray){
-  let sq = '';
-  let length = csvArray.length;   
-  for (let i = 0; i < length; i++) {
-    var endTimecode = tc.addTimes(csvArray[i].Timecode, addTime);
-    sq += (i + 1) + newline;
-    sq += (csvArray[i].Timecode + ',000' + ' --> ' + endTimecode) + ',000' + newline;
-    sq += csvArray[i].Note + newline;
-    sq += newline;
+  let srtSequence = '';
+
+  for (let i = 0; i < csvArray.length; i++) {
+    let endTimecode = tc.addTimes(csvArray[i].Timecode, addTime);
+    let srt = {};
+    srt.nr = (i + 1) + newline;
+    srt.timecode = csvArray[i].Timecode + ms + ' --> ' + endTimecode + ms + newline;
+    srt.caption = (i + 1) +' : '+ csvArray[i].Note + newline;
+    srt.newline = newline;
+    
+    srtSequence += (srt.nr + srt.timecode + srt.text + srt.newline);
   }
-  return sq;
+  return srtSequence;
 }
 
 // Sort csv array by timecode
